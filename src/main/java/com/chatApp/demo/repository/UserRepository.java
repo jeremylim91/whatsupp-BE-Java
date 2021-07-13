8a77710085b390434250f5077931a8f941a7eaf6
Jeremy <jeremylim_91@live.com>
2021-07-13 17:55:37 +0800

    changed userRepo

--- src/main/java/com/chatApp/demo/repository/UserRepository.java
+++ src/main/java/com/chatApp/demo/repository/UserRepository.java
@@ -1,9 +1,10 @@
-package com.chatApp.demo.repository;
-
-import com.chatApp.demo.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
+import java.util.List;
+//https://stackoverflow.com/questions/12730370/spring-data-mongodb-findby-method-for-nested-objects
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+    @Query(value = "{ 'userId' : ?0, 'questions.questionID' : ?1 }")
+    List<User> findUserInstanceByUsernameAndPassword(String username, String password);
+
+
 }
