5bfd53df057fc7ed065d429b067600647f86f047
Jeremy <jeremylim_91@live.com>
2021-07-16 10:25:09 +0800

    Added new code for custom query

--- src/main/java/com/chatApp/demo/repository/UserRepository.java
+++ src/main/java/com/chatApp/demo/repository/UserRepository.java
@@ -3,16 +3,22 @@ package com.chatApp.demo.repository;
 import com.chatApp.demo.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.Future;
 
 //https://stackoverflow.com/questions/12730370/spring-data-mongodb-findby-method-for-nested-objects
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
+    @Async
     @Query(value = "{ 'username' : ?0, 'password' : ?1 }")
     User findUserInstanceByUsernameAndPassword(String username, String password);
 
+    @Async
+    @Query(value = "{ 'username' : ?0}")
+    User findUserInstanceByUsername(String username);
 }
