af85a674115903a6cf70662f3ea081457d05e67f
jeremylim91 <jeremylim_91@live.com>
2021-07-22 08:06:14 +0800

    Refactored code

---
+++ src/main/java/com/chatApp/demo/seeders/SeederStrings.java
@@ -0,0 +1,12 @@
+package com.chatApp.demo.seeders;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SeederStrings {
+    public final String devHangout= "Dev hangout";
+    public final String cyclingBuddies= "Cycling buddies";
+    public final String foodiesUnite = "Foodies unite!";
+
+
+}
