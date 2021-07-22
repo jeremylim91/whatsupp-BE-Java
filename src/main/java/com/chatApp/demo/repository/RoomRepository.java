023d9753c08d0c8a456e0fdc0011f353da824295
jeremylim91 <jeremylim_91@live.com>
2021-07-22 08:07:40 +0800

    Added queries to support new routes

--- src/main/java/com/chatApp/demo/repository/RoomRepository.java
+++ src/main/java/com/chatApp/demo/repository/RoomRepository.java
@@ -1,9 +1,18 @@
 package com.chatApp.demo.repository;
 
+import com.chatApp.demo.model.Message;
 import com.chatApp.demo.model.Room;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RoomRepository extends MongoRepository <Room, String> {
+    Optional <Room> findById(String id);
+
+    List<Room> findAll();
+
+    Room insert(Room roomToInsert);
 }
