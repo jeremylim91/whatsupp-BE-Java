8f77cc119540e3ff0fe4e8c59c9b252f0c30cf76
jeremylim91 <jeremylim_91@live.com>
2021-07-22 08:06:33 +0800

    Refactored code

---
+++ src/main/java/com/chatApp/demo/controller/UserDetails.java
@@ -0,0 +1,35 @@
+package com.chatApp.demo.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class UserDetails {
+    private String username;
+    private String password;
+
+//    public UserDetails(String username, String password) {
+//        this.username = username;
+//        this.password = password;
+//    }
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+}
