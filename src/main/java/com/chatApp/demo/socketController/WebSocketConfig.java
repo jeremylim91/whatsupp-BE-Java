893bcec372dee2bda2c9d4cf5187e4bab1ad3afe
Tsang Jun Wen <junwen2992@gmail.com>
2021-08-04 14:15:44 +0800

    clean up redundant stomp endpoints and lint codes

--- src/main/java/com/chatApp/demo/socketController/WebSocketConfig.java
+++ src/main/java/com/chatApp/demo/socketController/WebSocketConfig.java
@@ -2,35 +2,26 @@ package com.chatApp.demo.socketController;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-//package com.chatApp.demo;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-//
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-@Override
+    @Override
     public void configureMessageBroker(MessageBrokerRegistry config){
 //   We enable an in-memory message broker to carry the messages back to the client on
 //   destinations prefixed with “/topic”
-    config.enableSimpleBroker("/wsFromServer");
+        config.enableSimpleBroker("/wsFromServer");
 //    We complete our simple configuration by designating the “/app” prefix to filter destinations targeting application annotated methods (via @MessageMapping).
-    config.setApplicationDestinationPrefixes("/wsToServer");
-}
-@Override
+        config.setApplicationDestinationPrefixes("/wsToServer");
+    }
+    @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-//    registry.addEndpoint("/chat");
-    registry.addEndpoint("/chat").setAllowedOrigins("*");
-    registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/chat")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
 
-}
+    }
 }
